e3a3d5903f177da82d2ca02c23da4c450d3c38fe
LPF33 <larspefe@gmail.com>
2021-03-18 20:33:32 +0100

    more stabile & points

--- public/script.js
+++ public/script.js
@@ -1,99 +1,108 @@
-const main = document.querySelector("main");
-const startButton = document.querySelector("#start-button");
-const socket = io.connect();
+(function () {
+    const main = document.querySelector("main");
+    const startButton = document.querySelector("#start-button");
+    const socket = io.connect();
 
-// socket.on("connect", () => {
-//     console.log("connected");
-// });
+    socket.emit("player-online");
 
-socket.emit("player-online");
-
-socket.on("flags", (data) => {
-    data.forEach((element, index) => {
-        main.innerHTML += `<img src="${element.flag}" class="flag" id="flag${index}"/>`;
+    socket.on("flags", (data) => {
+        data.forEach((element, index) => {
+            main.innerHTML += `<img src="${element.flag}" class="flag" id="flag${index}"/>`;
+        });
+        startButton.innerHTML = "BE PREPARED!";
     });
-    startButton.innerHTML = "READY ? CLICK !";
-});
 
-socket.on("send-player-emoji", (data) => {
-    document.body.style.cursor = startButton.style.cursor = `url("data:image/svg+xml;utf8,<svg xmlns='http://www.w3.org/2000/svg' width='40' height='60' viewport='0 0 100 100' style='fill:black;font-size:30px;'><text y='50%'>${data.emoji}</text></svg>") 16 0, auto`;
-    document.addEventListener("mousemove", function hello(e) {
-        socket.emit("player-mouse-position", {
-            ...data,
-            x: e.pageX,
-            y: e.pageY,
+    socket.on("send-player-emoji", (data) => {
+        document.body.style.cursor = startButton.style.cursor = `url("data:image/svg+xml;utf8,<svg xmlns='http://www.w3.org/2000/svg' width='40' height='60' viewport='0 0 100 100' style='fill:black;font-size:30px;'><text y='50%'>${data.emoji}</text></svg>") 16 0, auto`;
+        document.addEventListener("mousemove", function (e) {
+            socket.emit("player-mouse-position", {
+                ...data,
+                x: e.pageX,
+                y: e.pageY,
+            });
         });
     });
-});
 
-socket.on("player-mouse-position", (data) => {
-    const playerEmoji = document.querySelector("#player" + data.socketId);
-    if (playerEmoji) {
-        playerEmoji.style.left = data.x + "px";
-        playerEmoji.style.top = data.y + "px";
-        return;
-    }
-    const emojiDiv = document.createElement("div");
-    document.body.appendChild(emojiDiv);
-    emojiDiv.id = "player" + data.socketId;
-    emojiDiv.classList.add("emoji");
-    emojiDiv.innerHTML = data.emoji;
-    emojiDiv.style.left = data.x + "px";
-    emojiDiv.style.top = data.y + "px";
-});
+    socket.on("player-mouse-position", (data) => {
+        const playerEmoji = document.querySelector("#player" + data.socketId);
+        if (playerEmoji) {
+            playerEmoji.style.left = data.x + "px";
+            playerEmoji.style.top = data.y + "px";
+            return;
+        }
+        const emojiDiv = document.createElement("div");
+        document.body.appendChild(emojiDiv);
+        emojiDiv.id = "player" + data.socketId;
+        emojiDiv.classList.add("emoji");
+        emojiDiv.innerHTML = data.emoji;
+        emojiDiv.style.left = data.x + "px";
+        emojiDiv.style.top = data.y + "px";
+    });
 
-startButton.addEventListener(
-    "click",
-    function (e) {
-        socket.emit("start-game");
-        startButton.innerHTML = "WAIT !!!";
-    },
-    { once: true }
-);
+    const country = document.createElement("h3");
+    let correctFlag = null;
 
-const country = document.createElement("h3");
-let correctFlag = null;
+    socket.on("question", (data) => {
+        console.log(data);
+        startButton.style.display = "none";
+        main.prepend(country);
+        country.innerHTML = data.name;
+        document.addEventListener(
+            "click",
+            (event) => {
+                correctFlag = data.index;
+                if (event.target.id == data.index) {
+                    socket.emit("answered", "win");
+                } else {
+                    document.getElementById(data.index).style.border =
+                        "10px solid #b6fd8e";
+                    setTimeout(() => {
+                        document.getElementById(data.index).style.border = "";
+                    }, 1500);
+                    socket.emit("answered", "lose");
+                }
+            },
+            { once: true }
+        );
+    });
 
-socket.on("question", (data) => {
-    console.log(data);
-    startButton.style.display = "none";
-    main.prepend(country);
-    country.innerHTML = data.name;
-    document.addEventListener(
-        "click",
-        (event) => {
-            correctFlag = data.index;
-            if (event.target.id == data.index) {
-                socket.emit("end", "win");
-            } else {
-                document.getElementById(data.index).style.border =
-                    "4px solid black";
-                setTimeout(() => {
-                    document.getElementById(data.index).style.border = "";
-                }, 1500);
-                socket.emit("end", "lose");
-            }
-        },
-        { once: true }
-    );
-});
+    socket.on("send-points", () => {
+        socket.emit("send-points");
+    });
 
-socket.on("winner", (data) => {
-    country.innerHTML = "GOOD JOB: ";
-    if (data.length) {
-        data.forEach((item) => (country.innerHTML += item));
-    } else {
-        country.innerHTML = "No one knew it !!!";
-    }
-});
+    const pointsSection = document.getElementById("player-points");
+    socket.on("points", (data) => {
+        const pointEmojiElement = document.getElementById(
+            `points-${data.socketId}`
+        );
+        if (pointEmojiElement) {
+            pointEmojiElement.children[0].innerHTML = data.points;
+            return;
+        }
+        const pointElem = document.createElement("span");
+        pointElem.classList.add("points");
+        pointElem.id = `points-${data.socketId}`;
+        pointElem.innerHTML = `${data.emoji}<p>${data.points}</p>`;
+        pointsSection.appendChild(pointElem);
+    });
 
-socket.on("no-button", () => {
-    startButton.style.display = "none";
-});
+    socket.on("winner", (data) => {
+        country.innerHTML = "GOOD JOB: ";
+        if (data.length) {
+            data.forEach((item) => (country.innerHTML += item));
+        } else {
+            country.innerHTML = "No one knew it !!!";
+        }
+    });
 
-socket.on("socket-left", (data) => {
-    const playerEmoji = document.querySelector(`#player${data}`);
-    if (playerEmoji) {
-        document.body.removeChild(playerEmoji);
-    }
-});
+    socket.on("socket-left", (data) => {
+        const playerEmoji = document.querySelector(`#player${data}`);
+        if (playerEmoji) {
+            document.body.removeChild(playerEmoji);
+        }
+        const pointEmojiElement = document.getElementById(`points-${data}`);
+        if (pointEmojiElement) {
+            pointsSection.removeChild(pointEmojiElement);
+        }
+    });
+})();
--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const socketIO = require("socket.io");
-const flags = require("./countriesflags.json").sort(() => Math.random() - 0.5);
+const flagsArr = require("./countriesflags.json");
 
 // prettier-ignore
 const emojis = ['🐸','🐱','🦊', '🐻', '🐼', '🐨', '🐙', '🦁','🐹', '🐰'];
@@ -17,83 +17,99 @@ app.get("/", (request, response) => {
     response.sendFile(__dirname + "/views/index.html");
 });
 
-let players = 0;
-let playerCounter = 0;
-let playerGuess = [];
 const gameData = {
+    playerCounter: 0,
     started: false,
     flag: null,
+    playerGuess: [],
+    timeoutId: null,
+    flags: [],
 };
+
+function startGame(io, socket) {
+    if (gameData.started) {
+        socket.emit("question", {
+            ...gameData.flags[gameData.flag],
+            index: "flag" + gameData.flag,
+        });
+        return;
+    }
+    if (gameData.timeoutId) {
+        clearTimeout(gameData.timeoutId);
+    }
+    gameData.timeoutId = setTimeout(() => {
+        gameData.started = true;
+        gameData.flag = Math.floor(Math.random() * 48);
+        io.emit("question", {
+            ...gameData.flags[gameData.flag],
+            index: "flag" + gameData.flag,
+        });
+    }, 5000);
+}
 
 const io = socketIO(server);
 
 io.on("connection", (socket) => {
-    players = io.sockets.sockets.size;
-    const emoji = emojis.pop();
+    if (!emojis.length) {
+        socket.disconnect();
+    }
+    const playerData = {
+        emoji: emojis.pop(),
+        socketId: socket.id,
+        points: 0,
+    };
     console.log("socket is now connected", socket.id, emoji);
 
+    if (io.sockets.sockets.size === 1) {
+        gameData.flags = flagsArr.sort(() => Math.random() - 0.5).slice(0, 48);
+    }
+
     socket.on("player-online", () => {
-        const playerDate = {
-            emoji,
-            socketId: socket.id,
-        };
-        socket.emit("send-player-emoji", playerDate);
-        socket.emit("flags", flags.slice(0, 48));
-        if (gameData.started) {
-            socket.emit("no-button");
-            playerCounter++;
-            setTimeout(() => {
-                socket.emit("question", {
-                    ...flags[gameData.flag],
-                    index: "flag" + gameData.flag,
-                });
-            }, 1500);
-        }
+        socket.emit("send-player-emoji", playerData);
+        socket.emit("flags", gameData.flags);
+        startGame(io, socket);
     });
 
     socket.on("disconnect", () => {
         console.log(
-            `socket with the id: ${socket.id} has disconnected, emoji:${emoji}"`
+            `socket with the id: ${socket.id} has disconnected, emoji:${playerData.emoji}"`
         );
-        playerCounter--;
-        players = io.sockets.sockets.size;
-        emojis.push(emoji);
+        emojis.push(playerData.emoji);
         io.emit("socket-left", socket.id);
+        nextQuestion();
     });
 
     socket.on("player-mouse-position", (data) => {
         socket.broadcast.emit("player-mouse-position", data);
     });
 
-    socket.on("start-game", () => {
-        playerCounter++;
-        if (playerCounter === players && players >= 2 && !gameData.started) {
-            gameData.started = true;
-            gameData.flag = Math.floor(Math.random() * 48);
-            io.emit("question", {
-                ...flags[gameData.flag],
-                index: "flag" + gameData.flag,
-            });
+    socket.on("answered", (data) => {
+        gameData.playerCounter++;
+        console.log(gameData);
+        if (data === "win") {
+            gameData.playerGuess.push(playerData.emoji);
+            playerData.points++;
         }
+        nextQuestion();
     });
 
-    socket.on("end", (data) => {
-        console.log("playerCounter", playerCounter, players);
-        playerCounter--;
-        if (data === "win") {
-            playerGuess.push(emoji);
-        }
-        if (playerCounter <= 0) {
-            io.emit("winner", playerGuess);
-            playerCounter = io.sockets.sockets.size;
-            playerGuess = [];
+    socket.on("send-points", () => {
+        io.emit("points", playerData);
+    });
+
+    function nextQuestion() {
+        if (gameData.playerCounter >= io.sockets.sockets.size) {
+            io.emit("winner", gameData.playerGuess);
+            gameData.playerCounter = 0;
+            gameData.playerGuess = [];
             gameData.flag = Math.floor(Math.random() * 48);
+            io.emit("send-points");
             setTimeout(() => {
                 io.emit("question", {
-                    ...flags[gameData.flag],
+                    ...gameData.flags[gameData.flag],
                     index: "flag" + gameData.flag,
                 });
             }, 3000);
         }
-    });
+    }
 });

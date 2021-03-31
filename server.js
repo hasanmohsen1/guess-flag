53954438234bcb0e90ea68b6d240d33731b7424f
LPF33 <larspefe@gmail.com>
2021-03-31 17:53:11 +0200

    content security policy

--- server.js
+++ server.js
@@ -14,7 +14,24 @@ const server = require("http").createServer(app);
 
 server.listen(process.env.PORT || 8080);
 
-app.use(helmet());
+app.use(
+    helmet.contentSecurityPolicy({
+        directives: {
+            "img-src": ["'self'", "https://restcountries.eu/"],
+        },
+    })
+);
+app.use(helmet.dnsPrefetchControl());
+app.use(helmet.expectCt());
+app.use(helmet.frameguard());
+app.use(helmet.hidePoweredBy());
+app.use(helmet.hsts());
+app.use(helmet.ieNoOpen());
+app.use(helmet.noSniff());
+app.use(helmet.permittedCrossDomainPolicies());
+app.use(helmet.referrerPolicy());
+app.use(helmet.xssFilter());
+
 app.use(express.static("public"));
 
 app.get("/", (request, response) => {

9da9779e92298bde119371776da6a25f62f3c7dc
hasanmohsen1 <110456953+hasanmohsen1@users.noreply.github.com>
2024-12-17 23:28:47 +0100

    Update redis.js

--- redis.js
+++ redis.js
@@ -1,9 +1,8 @@
 const redis = require("redis");
 const { promisify } = require("util");
 
-const redisConfig = process.env.REDIS_URL || {
-    host: "localhost",
-    port: 6379,
+const redisConfig = {
+    url: process.env.REDIS_URL || "redis://127.0.0.1:6379"
 };
 
 const client = redis.createClient(redisConfig);
